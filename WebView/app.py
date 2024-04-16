6b9494f3577486080e1051dfc9b22340a0689239
Hemanka <hkanoujia02@gmail.com>
2024-04-16 19:54:08 -0400

    display the converage info on web

--- WebView/app.py
+++ WebView/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from read_bugs_csv import get_bug_cvs_data
+from web_coverage import grab_csv_data
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html', 
                             clang_data=get_bug_cvs_data('bugs/clang.csv'), 
                             cvc5_data=get_bug_cvs_data('bugs/cvc5.csv'), 
@@ -13,6 +14,11 @@ def hello_world():
                             java_data=get_bug_cvs_data('bugs/java.csv'),
                             qiskit_data=get_bug_cvs_data('bugs/qiskit.csv'),
                             z3_data=get_bug_cvs_data('bugs/z3.csv'))
+
+@app.route('/coverage')
+def coverage():
+    return render_template('coverage.html', 
+                           data=grab_csv_data('IntermediateResults/full_run/full_run_coverage.csv'))
 
 if __name__ == '__main__':
     app.run(debug=True)
